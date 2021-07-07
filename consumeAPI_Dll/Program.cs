6960bc42a1a0809afaa9f4111d773626c4513027
ARI NABABAN <arinababan123@gmail.com>
2021-07-07 13:32:38 +0700

    first commit

---
+++ consumeAPI_Dll/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using API;
+
+namespace consumeAPI_Dll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             void consume ()
+            {
+                API.API api = new API.API();
+                try
+                {
+                    var response = api.getJson("http://worldclockapi.com/api/json/est/now", "");
+                    Console.WriteLine(response);
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.ReadKey();
+                }
+            }
+            consume();
+        }
+    }
+}
